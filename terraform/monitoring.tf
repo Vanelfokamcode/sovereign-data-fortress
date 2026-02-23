96f94093533c1513448463b90eb0f143788ed271
Vanelfokamcode <108558185+Vanelfokamcode@users.noreply.github.com>
2026-02-23 22:05:54 +0100

    feat: grafana working with prometheus connection

--- terraform/monitoring.tf
+++ terraform/monitoring.tf
@@ -1,10 +1,10 @@
 # terraform/monitoring.tf
-# Monitoring infrastructure with Prometheus
 
-# Prometheus container
+# Prometheus
 resource "docker_container" "prometheus" {
   name  = "fortress-prometheus"
   image = docker_image.prometheus.image_id
+  user  = "nobody"
 
   ports {
     internal = 9090
@@ -23,13 +23,12 @@ resource "docker_container" "prometheus" {
 
   command = [
     "--config.file=/etc/prometheus/prometheus.yml",
-    "--storage.tsdb.path=/prometheus",
-    "--web.console.libraries=/etc/prometheus/console_libraries",
-    "--web.console.templates=/etc/prometheus/consoles"
+    "--storage.tsdb.path=/prometheus"
   ]
 
   networks_advanced {
     name = docker_network.fortress_network.name
+    aliases = ["prometheus"]  # Important: DNS alias
   }
 
   restart = "unless-stopped"
@@ -39,29 +38,45 @@ resource "docker_image" "prometheus" {
   name = "prom/prometheus:latest"
 }
 
-# Dagster metrics exporter
-resource "docker_container" "dagster_exporter" {
-  name  = "fortress-dagster-exporter"
-  image = docker_image.dagster_exporter.image_id
+# Grafana
+resource "docker_container" "grafana" {
+  name  = "fortress-grafana"
+  image = docker_image.grafana.image_id
 
   ports {
-    internal = 8000
-    external = 8000
+    internal = 3000
+    external = 3001
+  }
+
+  env = [
+    "GF_SECURITY_ADMIN_USER=admin",
+    "GF_SECURITY_ADMIN_PASSWORD=fortress2024",
+    "GF_INSTALL_PLUGINS=",
+    "GF_SERVER_ROOT_URL=http://localhost:3001"
+  ]
+
+  volumes {
+    host_path      = "${path.cwd}/../data/grafana"
+    container_path = "/var/lib/grafana"
+  }
+
+  volumes {
+    host_path      = "${path.cwd}/../monitoring/grafana/provisioning"
+    container_path = "/etc/grafana/provisioning"
   }
 
   networks_advanced {
     name = docker_network.fortress_network.name
+    aliases = ["grafana"]  # Important: DNS alias
   }
 
   restart = "unless-stopped"
+  
+  depends_on = [docker_container.prometheus]
 }
 
-resource "docker_image" "dagster_exporter" {
-  name = "fortress-dagster-exporter:latest"
-  build {
-    context    = "${path.cwd}/../monitoring/exporters"
-    dockerfile = "Dockerfile"
-  }
+resource "docker_image" "grafana" {
+  name = "grafana/grafana:latest"
 }
 
 # Outputs
@@ -70,7 +85,7 @@ output "prometheus_url" {
   description = "Prometheus UI URL"
 }
 
-output "dagster_exporter_metrics_url" {
-  value       = "http://localhost:8000/metrics"
-  description = "Dagster metrics endpoint"
+output "grafana_url" {
+  value       = "http://localhost:3001"
+  description = "Grafana UI URL (admin/fortress2024)"
 }
